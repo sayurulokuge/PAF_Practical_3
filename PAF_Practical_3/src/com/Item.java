43f0a2053a4c3e11c5c133e6217125f3e1bbd972
sayuru lokuge <sayurulokuge@gmail.com>
2021-02-27 21:52:15 +0530

    Insert method created

--- PAF_Practical_3/src/com/Item.java
+++ PAF_Practical_3/src/com/Item.java
@@ -1,5 +1,24 @@
 package com;
+import java.sql.*;
 
 public class Item {
+	public Connection connect()
+	{
+	 Connection con = null;
 
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+	 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "");
+	 
+	 //To test the connection
+	 System.out.print("Successfully connected");
+	 }
+	 catch(Exception e)
+	 {
+	 e.printStackTrace();
+	 }
+
+	 return con;
+	}
 }
