2443fc1d0c2d9c35e1164455825aca6ca5ff06a1
sayuru lokuge <sayurulokuge@gmail.com>
2021-02-27 22:04:27 +0530

    Form created

--- PAF_Practical_3/src/com/Item.java
+++ PAF_Practical_3/src/com/Item.java
@@ -13,12 +13,48 @@ public class Item {
 	 
 	 //To test the connection
 	 System.out.print("Successfully connected");
-	 }
-	 catch(Exception e)
-	 {
-	 e.printStackTrace();
+	 
+	 } catch(Exception e) {
+		 e.printStackTrace();
 	 }
 
 	 return con;
 	}
-}
+	
+	public String insertItem(String code, String name, String price, String desc) {
+	
+		String output = "";
+		
+		try {
+	
+			Connection con = connect();
+			
+			if (con == null) {
+				return "Error while connecting to the database";
+			}
+			
+			// creating a prepared statement
+			String query = " insert into items(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)" + " values (?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc); 
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Inserted successfully";
+			
+		} catch (Exception e) {
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
+	}
+}	
+
